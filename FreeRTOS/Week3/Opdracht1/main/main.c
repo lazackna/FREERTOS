00b81afcd3120742b4da3271eaff5e34f89474be
David <davidstulemeijer@gmail.com>
2022-04-13 12:29:50 +0200

    Update 3.1

--- FreeRTOS/Week3/Opdracht1/main/main.c
+++ FreeRTOS/Week3/Opdracht1/main/main.c
@@ -8,20 +8,18 @@
 #include "freertos/semphr.h"
 #include "driver/gpio.h"
 
-#define N 5
+#define numPhil 5
 #define THINKING 2
 #define HUNGRY 1
 #define EATING 0
 #define LEFT (phnum + 4) % N
 #define RIGHT (phnum + 1) % N
 
-int state[N];
+int state[numPhil];
 
 TaskHandle_t threads[5];
 SemaphoreHandle_t buttonMutex;
-SemaphoreHandle_t S[N];
-
-int currentPhil = 0;
+SemaphoreHandle_t mutex[numPhil];
 
 int ledPins[5] = {12,14,27,26,25};
 
@@ -38,11 +36,9 @@ int eat(int phnum) {
 
         printf("Philosopher %d takes fork %d and %d\n",
                       phnum + 1, LEFT + 1, phnum + 1);
- 
         printf("Philosopher %d is Eating\n", phnum + 1);
         fflush(stdout);
 
-        //xSemaphoreGive(S[phnum]);
         return 0;
     }
     return 1;
@@ -71,42 +67,26 @@ void put_fork(int phnum) {
 void take_fork(int phnum) {
 
     xSemaphoreTake(buttonMutex, portMAX_DELAY);
-
     state[phnum] = HUNGRY;
-
     printf("Philosopher %d is Hungry\n", phnum + 1);
 
     int taken = eat(phnum);
-
     xSemaphoreGive(buttonMutex);
-    //if(taken)
-    //xSemaphoreTake(S[phnum], portMAX_DELAY);
 
     vTaskDelay(1);
 }
 
 void Philosopher(void * parameter) {
     
-    // int * num = (int*)parameter;
-    int myNumber;
-    myNumber = currentPhil;
-    currentPhil++;
-
-    gpio_reset_pin(ledPins[myNumber]);
-    gpio_set_direction(ledPins[myNumber], GPIO_MODE_OUTPUT);
+    gpio_reset_pin(ledPins[philId]);
+    gpio_set_direction(ledPins[philId], GPIO_MODE_OUTPUT);
     
     while(1) {
         vTaskDelay(10);
-
-        take_fork(myNumber);
-
+        take_fork(philId);
         vTaskDelay(0);
-
-        put_fork(myNumber);
+        put_fork(philId);
     }
-
-
-    
     vTaskDelete(NULL);
 }
 
@@ -115,13 +95,8 @@ void Philosopher(void * parameter) {
 void app_main(void)
 {
     buttonMutex = xSemaphoreCreateMutex();
-    for(int i = 0; i < 5; i++) {
-        // char * phil = "Philosopher: ";
-        // char taskName[1];
-        // itoa(i, taskName, 10);
-        // strcat(phil, taskName);
-        // printf(taskName);
-        S[i] = xSemaphoreCreateMutex();
+    for(int i = 0; i < numPhil; i++) {
+        mutex[i] = xSemaphoreCreateMutex();
         xTaskCreatePinnedToCore(Philosopher, "PhilosopherTask", 10000, NULL, 6, &threads[i], 0);
         vTaskDelay(10);
     }
