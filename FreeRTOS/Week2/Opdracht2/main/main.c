ab47ac26f41cd032116ef93a69ae264d77de2bb6
David <davidstulemeijer@gmail.com>
2022-04-13 12:09:09 +0200

    upgraded 2.2

--- FreeRTOS/Week2/Opdracht2/main/main.c
+++ FreeRTOS/Week2/Opdracht2/main/main.c
@@ -5,66 +5,51 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 
-unsigned int rLed = 32;
-unsigned int gLed = 33;
+unsigned int rLedPin = 32;
+unsigned int gLedPin = 33;
 
-TaskHandle_t rLedTask;
-TaskHandle_t lLedTask;
-
-TickType_t rLastWakeTime;
-TickType_t gLastWakeTime;
-BaseType_t rWasDelayed;
-BaseType_t gWasDelayed;
-
-TaskHandle_t noise1Task;
-TaskHandle_t noise2Task;
-TickType_t noise1LastWakeTime;
-TickType_t noise2LastWakeTime;
-BaseType_t noise1WasDelayed;
-BaseType_t noise2WasDelayed;
-
+//will make the red led blink at 0.5 seconds interval
 void Rood (void * parameter) {
-    rLastWakeTime = xTaskGetTickCount();
-    gpio_reset_pin(rLed);
-    gpio_set_direction(rLed, GPIO_MODE_OUTPUT);
+    TickType_t rLastWakeTime = xTaskGetTickCount();
+    gpio_reset_pin(rLedPin);
+    gpio_set_direction(rLedPin, GPIO_MODE_OUTPUT);
     while (1)
     {
-        gpio_set_level(rLed, 0);
-    
+        gpio_set_level(rLedPin, 0);
         vTaskDelayUntil(&rLastWakeTime, 500 / portTICK_PERIOD_MS);
-
-        gpio_set_level(rLed, 1);
-
+        gpio_set_level(rLedPin, 1);
         vTaskDelayUntil(&rLastWakeTime, 500 / portTICK_PERIOD_MS);
     }
 }
 
+//will make the green led blink at 2 seconds interval
 void Groen (void * parameter) {
-    gLastWakeTime = xTaskGetTickCount();
-    gpio_reset_pin(gLed);
-    gpio_set_direction(gLed, GPIO_MODE_OUTPUT);
+    TickType_t gLastWakeTime = xTaskGetTickCount();
+    gpio_reset_pin(gLedPin);
+    gpio_set_direction(gLedPin, GPIO_MODE_OUTPUT);
     while (1)
     {
-        gpio_set_level(gLed, 0);
+        gpio_set_level(gLedPin, 0);
         vTaskDelayUntil(&gLastWakeTime, 2000 / portTICK_PERIOD_MS);
-        gpio_set_level(gLed, 1);
+        gpio_set_level(gLedPin, 1);
         vTaskDelayUntil(&gLastWakeTime, 2000 / portTICK_PERIOD_MS);
     }
 }
 
+//will print noise string every 0.4 seconds
 void Noise1 (void * parameter) {
-    noise1LastWakeTime = xTaskGetTickCount();
+    TickType_t noise1LastWakeTime = xTaskGetTickCount();
 
     while (1)
     {
        printf("noisenoisenoisenoisenoisenoisenoisenoisenoisenoisenoisenoise");
        vTaskDelayUntil(&noise1LastWakeTime, 400 / portTICK_PERIOD_MS);
     }
-    
 }
 
+//will print noise string every 0.6 seconds
 void Noise2 (void * parameter) {
-    noise2LastWakeTime = xTaskGetTickCount();
+    TickType_t noise2LastWakeTime = xTaskGetTickCount();
 
     while (1)
     {
@@ -75,9 +60,9 @@ void Noise2 (void * parameter) {
 
 void app_main(void)
 {
-    xTaskCreatePinnedToCore(Rood, "Task1", 10000, NULL, 6, &rLedTask, 0);
-    xTaskCreatePinnedToCore(Groen, "Task2", 10000, NULL, 6, &lLedTask, 1);
+    xTaskCreatePinnedToCore(Rood, "Task1", 10000, NULL, 6, NULL, 0);
+    xTaskCreatePinnedToCore(Groen, "Task2", 10000, NULL, 6, NULL, 1);
 
-    xTaskCreatePinnedToCore(Noise1, "NoiseTask1", 10000, NULL, 6, &noise1Task, 0);
-    xTaskCreatePinnedToCore(Noise2, "NoiseTask2", 10000, NULL, 6, &noise2Task, 1);
+    xTaskCreatePinnedToCore(Noise1, "NoiseTask1", 10000, NULL, 6, NULL, 0);
+    xTaskCreatePinnedToCore(Noise2, "NoiseTask2", 10000, NULL, 6, NULL, 1);
 }
